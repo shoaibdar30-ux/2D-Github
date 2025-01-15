1f3aa0f65ea2a7df8d94da187047a700654b8b41
shoaibdar30-ux <shoaibabdullahdarkashmiri@gmail.com>
2025-01-15 13:28:18 +0500

    Added Input Of Axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
 {  public Rigidbody2D rigidbody2d;
@@ -10,6 +11,29 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetAxis("Horizontal") > 0) //It Is Positive(+ve)
+        {
+            rigidbody2d.linearVelocity = new Vector2(speed, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0) //It Is Negative(-ve)
+        {
+            rigidbody2d.linearVelocity = new Vector2(-speed, 0f);
+        }
+        else if (Input.GetAxis("Vertical") > 0) //It Is Positive(+ve)
+        {
+            rigidbody2d.linearVelocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0) //It Is Negative(-ve)
+        {
+            rigidbody2d.linearVelocity = new Vector2(0f, -speed);
+        }
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+                {
+             //stop
+        }
+
+                   
         
+
     }
 }
