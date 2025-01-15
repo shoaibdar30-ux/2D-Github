85cac7f471854890d7a0c293d815779f0596030c
shoaibdar30-ux <shoaibabdullahdarkashmiri@gmail.com>
2025-01-15 14:32:00 +0500

    Debug.Log Feature
    Added a Debug.log (Level Completed) when sqaure finish at Finish Line(Door)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,13 @@
+using System;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
 {  public Rigidbody2D rigidbody2d;
    public float speed;
-    void Start()
-    {
-        
-    }
+
 
     void Update()
     {
@@ -28,12 +28,15 @@ public class PlayerController : MonoBehaviour
             rigidbody2d.linearVelocity = new Vector2(0f, -speed);
         }
         else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
-                {
-             rigidbody2d.linearVelocity = new Vector2(0f,0f);
+        {
+            rigidbody2d.linearVelocity = new Vector2(0f, 0f);
         }
-
-                   
-        
-
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Level Completed!!!!");
+    }
+
+
 }
+
